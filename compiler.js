27dae01956e93c3464799ea7a0704d1dfc071cd2
Dominick Gendill <dgendill@gmail.com>
2012-04-17 18:54:29 -0600

    Updated jsdoc

--- compiler.js
+++ compiler.js
@@ -1,37 +1,41 @@
 /**
- * Compiles the made up music language MUS into the madeup music language NOTE.
- *
- * @param {Object} musexpr An object structured in the MUS Language.
- * @return {Object} Returns an object sructured in the NOTE Language.
- */
-var compile = function (musexpr) {
-    var time = 0;
-    var notes = [];
-    var duration = 0;
-    
-    var section = function(expr, increment) {
-		if (expr.tag === 'seq') {
-			section(expr.left, true);
-			section(expr.right, true);
-		} else if (expr.tag === 'par') {
-			section(expr.left, false);
-			section(expr.right, true);
-		} else {			
-			var toReturn = {
-				tag:expr.tag,
-				pitch:expr.pitch,
-				start:time,
-				dur:expr.dur
-			};
-			//duration = (expr.dur > duration) ? expr.dur : duration;
-			if (increment) {
-				time = time + expr.dur;
-				//duration = 0;				
-			}
-			notes.push(toReturn);
-		}
+  * @module
+  */
+var compiler = function() {
+   /**
+    * Compiles the made up music language MUS into the madeup music language NOTE.
+    *
+    * @param {Object} musexpr An object structured in the MUS Language.
+    * @returns {Object} Returns an object sructured in the NOTE Language.
+    */
+  this.compile = function (musexpr) {
+      var time = 0;
+      var notes = [];
+      var duration = 0;
+      var section = function(expr, increment) {
+          if (expr.tag === 'seq') {
+              section(expr.left, true);
+              section(expr.right, true);
+          } else if (expr.tag === 'par') {
+              section(expr.left, false);
+              section(expr.right, true);
+          } else {			
+              var toReturn = {
+                  tag:expr.tag,
+                  pitch:expr.pitch,
+                  start:time,
+                  dur:expr.dur
+              };
+              if (increment) {
+                  time = time + expr.dur;
+              }
+              notes.push(toReturn);
+          }
+      };
+      section(musexpr);
+      return notes;
     };
-    section(musexpr);
-    return notes;
-};
+    
+  };
+ }
 
